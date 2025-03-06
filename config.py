275ddd11bc4ec6dac342726261cfbf4eb2b40e9d
Don Berndt <43327350+dberndt-usf@users.noreply.github.com>
2025-03-06 17:46:19 -0500

    Added Config File Headers
    Added config file headers to better document usage.

--- config.py
+++ config.py
@@ -7,8 +7,10 @@ def config_fred_api(config_path, verbosity):
     # Read config file for FRED API key.
     if verbosity > 0:
         print("\nConfiguring FRED API ...")
-    with open(config_path + 'fred_flow.csv', 'r') as file:
+    with open(config_path + 'fredflow_config.csv', 'r') as file:
         csvreader = csv.reader(file)
+        # Skip headers on first line.
+        next(csvreader)
         for row in csvreader:
             if row[0] == 'api_key':
                 fredapi_key = row[1]
@@ -24,6 +26,8 @@ def config_fred_series(config_path, verbosity):
         print("\nConfiguring FRED series dictionary ...")
     with open(config_path + 'fred_series.csv', 'r') as file:
         csvreader = csv.reader(file)
+        # Skip headers on first line.
+        next(csvreader)
         for row in csvreader:
             fred_dict[row[0]] = row[1]
     return fred_dict
@@ -38,6 +42,8 @@ def config_fred_granularity(config_path, verbosity):
         print("\nConfiguring FRED granularity dictionary ...")
     with open(config_path + 'fred_series.csv', 'r') as file:
         csvreader = csv.reader(file)
+        # Skip headers on first line.
+        next(csvreader)
         for row in csvreader:
             granularity_dict[row[0]] = row[2]
     return granularity_dict
